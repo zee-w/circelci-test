e893cc0dd5c0bbebe0063c214b45f339ba2b195b
zee-w <zeeshan.wani10@gmail.com>
2020-11-17 11:55:28 +0530

    added a new branch

--- cypress/integration/02_cmsPages.spec.js
+++ cypress/integration/02_cmsPages.spec.js
@@ -1,6 +1,6 @@
 describe("Cms Suite", ()=>{
     it("Cms Case2", ()=>{
-        cy.log('Visiting Google Second time')
+        cy.log('Visiting Google Second time222')
         cy.visit('https://www.google.com')
     })
 })
